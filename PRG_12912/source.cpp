5915ec68e187924cbd1f69c0cdf45a620e7a24a3
umspirit <umjugnki@gmail.com>
2019-09-28 14:12:49 +0900

    Commit

---
+++ PRG_12912/source.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+	long long answer = 0;
+
+	if (a > b) {
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+
+	for (int i = a; i <= b; i++) {
+		answer += (long long)i;
+	}
+
+	return answer;
+}
